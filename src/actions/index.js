4d43237b13563ed44a22094cc03890228849864d
Miikka Värri <miikka.varri@gmail.com>
2018-11-16 22:59:21 +0100

    Tabs to spaces.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,22 @@
 import { NOTES, CATEGORY, SEARCH } from '../constants';
 
 export const getNotes = (notes, categories) => ({
-	type: NOTES.GET_NOTES, 
-	notes, 
-	categories
+  type: NOTES.GET_NOTES, 
+  notes, 
+  categories
 });
 
 export const getCategories = (categories) => ({
-	type: CATEGORY.GET_CATEGORIES, 
-	categories
+  type: CATEGORY.GET_CATEGORIES, 
+  categories
 });
 
 export const selectCategory = (uid) => ({
-	type: CATEGORY.SELECT_CATEGORY, 
-	uid
+  type: CATEGORY.SELECT_CATEGORY, 
+  uid
 });
 
 export const setSearch = (query) => ({
-	type: SEARCH.SET_SEARCH, 
-	query
+  type: SEARCH.SET_SEARCH, 
+  query
 });
