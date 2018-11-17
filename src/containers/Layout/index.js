fc8e3ff601f99036e7db80d85f5724ffcdcaaf00
Miikka Värri <miikka.varri@gmail.com>
2018-11-17 12:12:08 +0100

    Layout component is now SFC

--- src/containers/Layout/index.js
+++ src/containers/Layout/index.js
@@ -1,24 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route } from "react-router-dom";
 import { Note } from '../';
 import { Header } from '../../components';
 import { Grid, Row, Col } from 'react-bootstrap';
 
-class Layout extends Component {
-
-  render() {
-    
-    return (
-      <Grid fluid={ true }>
-        <Row className="show-grid header">
-          <Col xs={12} md={12}>
-            <Header />
-          </Col>
-        </Row>
-        <Route path="/:id?" component={Note} />
-      </Grid>
-    );
-  }
-}
+const Layout = () => (
+  <Grid fluid={ true }>
+    <Row className={'show-grid header'}>
+      <Col xs={12} md={12}>
+        <Header />
+      </Col>
+    </Row>
+    <Route path="/:id?" component={Note} />
+  </Grid>
+)
 
 export default Layout;
