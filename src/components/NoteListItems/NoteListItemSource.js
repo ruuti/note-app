afe033d436480f9cdbfb530316b3f9de10fe2ef2
Miikka Värri <miikka.varri@gmail.com>
2018-11-18 01:48:04 +0100

    Code styling.

--- src/components/NoteListItems/NoteListItemSource.js
+++ src/components/NoteListItems/NoteListItemSource.js
@@ -3,21 +3,20 @@ import { DragSource } from 'react-dnd';
 import { moveNoteToCategory }  from '../../firebase';
 import { NOTE_ITEM } from '../../constants';
 
-const NoteListItemSource = ({connectDragSource, isDragging, children}) => {
-
-  return connectDragSource(
+const NoteListItemSource = ({dragSource, isDragging, children}) => (
+  dragSource(
     <div className={ isDragging ? 'isDragging' : undefined}>
       { children }
     </div>
   )
-}
+)
 
 const source = {
   beginDrag(props) {
     return ({});
   },
   endDrag(props, monitor) {
-    if (!monitor.didDrop()) {
+    if(!monitor.didDrop()) {
       return;
     }
     const { note } = props;
@@ -25,9 +24,10 @@ const source = {
     moveNoteToCategory(note.id, category.id);
   }
 };
+
 const collect = (connect, monitor) => ({
-  connectDragSource: connect.dragSource(),
-  isDragging: monitor.isDragging(),
+  dragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
 });
 
 export default DragSource(NOTE_ITEM, source, collect)(NoteListItemSource);
