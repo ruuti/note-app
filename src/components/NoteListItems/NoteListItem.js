ac1eb22c46a2424179928c8b4cc788bc5221b6b6
Miikka Värri <miikka.varri@gmail.com>
2018-11-18 16:58:37 +0100

    Code refactoring

--- src/components/NoteListItems/NoteListItem.js
+++ src/components/NoteListItems/NoteListItem.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import { ListGroupItem } from 'react-bootstrap';
 import { Link, withRouter } from "react-router-dom";
-
 import NoteListItemContent from './NoteListItemContent';
 import NoteListItemSource from './NoteListItemSource';
-
 import { removeNote }  from '../../firebase';
 import { IconButton } from '../';
 
@@ -12,26 +10,28 @@ class NoteListItem extends Component {
   
   constructor(props) {
     super(props);
-
-    this.state = { 
-      selectedNote : props.match.params.id
-    };
+    this.state = { selectedNote: props.match.params.id };
     this.handleRemove = this.handleRemove.bind(this);
   }
 
+  /**
+   * Remove note
+   */
   handleRemove(){
     removeNote(this.props.note.id);
   }
 
+  /**
+   * Listen location changes to detect when new note is loaded
+   */
   componentDidMount() {
-    this.unlisten = this.props.history.listen(location => {
-      // TODO: change this.
-      this.setState({
-        selectedNote: location.pathname.substring(1)
-      });  
-    });
+    this.unlisten = this.props.history.listen(({ pathname }) =>
+      this.setState({ selectedNote: pathname.substring(1) }));
   }
 
+  /**
+   * Stop listening location changes
+   */
   componentWillUnmount() {
     this.unlisten();
   }
